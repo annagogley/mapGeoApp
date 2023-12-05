6e9abbc5018d0a860cf860af777d376ee7ef9543
Anna Vorontsova <60662201+annagogley@users.noreply.github.com>
2023-12-05 18:52:10 +0300

    added bottom sheet

--- mapGeoApp/ContentView.swift
+++ mapGeoApp/ContentView.swift
@@ -15,48 +15,55 @@ struct ContentView: View {
     @State private var pinTapped = FriendLocation(name: "", imageName: "", lastSeenDate: "§", lastSeenTime: "", location: CLLocationCoordinate2D())
     
     var body: some View {
-        Map(coordinateRegion: $mapRegion, showsUserLocation: true, annotationItems: vm.mockLocations) { loc in
-            MapAnnotation(coordinate: loc.location) {
-                ZStack {
-                    Image(ImageNameConstant.tracker)
-                        .resizable()
-                        .frame(width: 100, height:  100)
-                    Image(loc.imageName)
-                        .resizable()
-                        .scaledToFit()
-                        .cornerRadius(60)
-                        .frame(width: 70,
-                               height: 70)
-                        .clipped()
-                        .padding(.bottom, 15)
-                }
-                .onTapGesture {
-                    showBottomSheet.toggle()
-                    vm.pinButtonTapped(loc: loc)
-                    print("PinTapped", showBottomSheet, pinTapped)
+        ZStack {
+            Map(coordinateRegion: $mapRegion, showsUserLocation: true, annotationItems: vm.mockLocations) { loc in
+                MapAnnotation(coordinate: loc.location) {
+                    ZStack {
+                        Image(ImageNameConstant.tracker)
+                            .resizable()
+                            .frame(width: 100, height:  100)
+                        Image(loc.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(60)
+                            .frame(width: 70,
+                                   height: 70)
+                            .clipped()
+                            .padding(.bottom, 15)
+                    }
+                    .onTapGesture {
+                        showBottomSheet.toggle()
+                        vm.pinButtonTapped(loc: loc)
+                        print("PinTapped", showBottomSheet, pinTapped)
+                    }
                 }
             }
-        }
-        .onReceive(vm.$mapRegion, perform: { _ in
-            mapRegion = vm.mapRegion
-        })
-        .onReceive(vm.$pinTapped, perform: { _ in
-            pinTapped = vm.pinTapped
-        })
-        .ignoresSafeArea()
-        .sheet(isPresented: $showBottomSheet) {
-            VStack {
-                Text(pinTapped.imageName)
+            .onReceive(vm.$mapRegion, perform: { _ in
+                mapRegion = vm.mapRegion
+            })
+            .onReceive(vm.$pinTapped, perform: { _ in
+                pinTapped = vm.pinTapped
+            })
+            .ignoresSafeArea()
+            .sheet(isPresented: $showBottomSheet) {
+                SheetView(friendTapped: pinTapped)
+                    .presentationDetents([.height(200)])
             }
+            .onAppear(perform: vm.checkIfLocationServicesAreEnabled)
         }
-        
-        .onAppear(perform: vm.checkIfLocationServicesAreEnabled)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(vm: ContentView.ContentViewModel())
+//        SheetView(friendTapped: FriendLocation(
+//            name: "Ленка",
+//            imageName: FriendPhoto.lenka,
+//            lastSeenDate: "04.12.23",
+//            lastSeenTime: "15:00",
+//            location: CLLocationCoordinate2D(latitude: 59.937, longitude: 30.309)
+//        ))
     }
 }
 
@@ -154,5 +161,60 @@ extension ContentView {
             pinTapped = loc
         }
         
+    }
+}
+
+
+struct SheetView: View {
+    @State var friendTapped : FriendLocation
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                ZStack {
+                    Circle()
+                        .frame(width: 90)
+                        .foregroundColor(.blue)
+                    Image(friendTapped.imageName)
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(45)
+                }
+                VStack(alignment: .leading) {
+                    Text(friendTapped.name)
+                        .font(.title2)
+                        .bold()
+                    HStack {
+                        PropertyView(iconName: "wifi", text: "GPS")
+                        PropertyView(iconName: "calendar", text: friendTapped.lastSeenDate)
+                        PropertyView(iconName: "clock", text: friendTapped.lastSeenTime)
+                    }
+                }
+            }
+            Button {
+                print("button tapped")
+            } label: {
+                ZStack {
+                    Capsule()
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 46)
+                    Text("Посмотреть историю")
+                        .foregroundColor(.white)
+                        .bold()
+                }
+            }
+
+        }
+    }
+}
+
+struct PropertyView: View {
+    @State var iconName : String
+    @State var text : String
+    var body: some View {
+        HStack(spacing: 4) {
+            Image(uiImage: UIImage(systemName: iconName)!)
+                .renderingMode(.template)
+                .foregroundColor(.blue)
+            Text(text)
+        }
     }
 }
